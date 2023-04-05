c266e18d5fe9754b7b1c6bdc1805a05f90703550
mirko <mirkmarkese@gmail.com>
2023-04-05 15:19:49 +0200

    1

--- src/main/java/co/develhope/logging/controllers/BasicController.java
+++ src/main/java/co/develhope/logging/controllers/BasicController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,9 @@ public class BasicController {
     }
     @GetMapping("/get-error")
     public void getError(){
-        logger.error("error");
+        Error error= new Error("error");
+        logger.error(error.getMessage());
+
     }
 
 }
